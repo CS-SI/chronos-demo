8316111c8483d6eb5548071a98efbb5cbfa3bbbc
ctriquet-cs <christophe.triquet@c-s.fr>
2018-10-18 11:57:13 +0200

    Enhance init scripts

--- init_args.py
+++ init_args.py
@@ -4,17 +4,19 @@ import sys
 import json
 
 
-INPUT_FILES_NB = 150
 BASE_PATH = sys.argv[1] if len(sys.argv) > 1 else '/data/toil/input'
 
 if __name__ == '__main__':
     input_args = dict()
     input_args['L0B_HR_RAW_150'] = []
 
-    for i in range(INPUT_FILES_NB):
+    files = os.listdir(BASE_PATH)
+    for i in sorted(files):
         input_file = {'class': 'File',
-                      'path': os.path.join(BASE_PATH, 'infile{:03}.txt').format(i + 1)}
+                      'path': os.path.join(BASE_PATH, i)}
         input_args['L0B_HR_RAW_150'].append(input_file)
     with open('args.json', 'wt') as fd:
         json.dump(input_args, fd)
+
+    print('args.json generated with link to {} files'.format(len(files)))
 
--- init_input.sh
+++ init_input.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-INPUT_DIR=${1:-/data/toil/input}
+INPUT_NB=${1:-150}
+INPUT_DIR=${2:-/data/toil/input}
 mkdir -p $INPUT_DIR
-for i in {001..150}
+for i in `seq 1 ${INPUT_NB}`
 do
-    echo 'hello world' > ${INPUT_DIR}/infile${i}.txt
+	echo 'hello world' > ${INPUT_DIR}/infile$(printf "%03d" ${i}).txt
 done
+echo "${INPUT_NB} generated into ${INPUT_DIR}"
+exit 0
